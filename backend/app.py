515a065544be3e49dadc048f67ef4f1bb044069e
elkanajnrmanullang <elkanamanullang7@gmail.com>
2025-08-14 23:13:16 +0700

    fix bisa yok

--- backend/app.py
+++ backend/app.py
@@ -28,6 +28,8 @@ app = Flask(
 )
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
+app.secret_key = os.getenv("SECRET_KEY", "default_secret_key")
+
 
 UPLOAD_FOLDER = '../uploads'
 OUTPUT_FOLDER = '../outputs'
